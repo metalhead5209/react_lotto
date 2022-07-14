841b347200c4d93d3ccb2bfa3b1d121e3024053c
Aaron Rodi <metalhead5209@gmail.com>
2022-07-13 19:50:42 -0500

    created boilerplate. added ball component

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Ball from './components/Ball';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Ball num={30}/>
+      <Ball num={8}/>
+      <Ball num={14}/>
+      <Ball num={29}/>
+      <Ball num={12}/>
+      <Ball num={25}/>
+    </div>
+  )
+}
+
+export default App
